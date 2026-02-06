b19787ef0aacba89901248f6301bf5c454d30c0c
Sadeepa Dinakara <sadeepa14dinakara@gmail.com>
2026-02-06 14:32:46 +0530

    mongo db connection setupd with url

--- New_Sirini_Hotel/Backend/src/server.js
+++ New_Sirini_Hotel/Backend/src/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const connectDB = require('./config/db');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/mongoconnection");
 
 dotenv.config();
 connectDB();
@@ -13,8 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('API is running...');
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
 const PORT = process.env.PORT || 5000;
