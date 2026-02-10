1a9f354629cf033ff2be3b33712c10560ab4f891
Sadeepa Dinakara <sadeepa14dinakara@gmail.com>
2026-02-10 20:35:30 +0530

    router add and register login frontend implement

--- New_Sirini_Hotel/Frontend/src/App.jsx
+++ New_Sirini_Hotel/Frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import { Routes, Route } from "react-router-dom";
+import Login from './pages/Login';
+import Registration from './pages/Registration';
 export const App = () => {
   return (
-    <h1>Hello</h1>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Registration />} />
+    </Routes>
   )
 }
-
 export default App;
