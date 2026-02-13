55162d746eaaf9360f979412c4fd15dc0731dcbf
Dilshan0721 <kavindu_20221359@fot.ruh.ac.lk>
2026-02-13 14:34:15 +0530

    Add text to the main room image

--- New_Sirini_Hotel/Frontend/src/Pages/ServicesPages/Rooms.jsx
+++ New_Sirini_Hotel/Frontend/src/Pages/ServicesPages/Rooms.jsx
@@ -12,6 +12,35 @@ function Rooms() {
           alt="Hotel Room Header"
           className="absolute inset-0 w-full h-full object-cover z-0"
         />
+
+         {/* Dark Overlay for main room image*/}
+        <div className="absolute inset-0 bg-black/40 z-10" />
+
+        {/* Main room image text */}
+        <div className="relative z-20 px-4 text-center">
+      
+     
+      <h1 className="text-6xl md:text-7xl font-serif mb-4 drop-shadow-lg 
+      transition-all duration-700 hover:scale-110 hover:text-yellow-400 cursor-pointer">
+        Rooms
+      </h1>
+
+     
+      <p className="italic text-xl md:text-2xl font-light tracking-wide drop-shadow-md 
+      transition-all duration-700 hover:tracking-widest">
+        Peaceful rooms designed for your perfect stay
+      </p>
+
+      
+      <button className="mt-8 px-8 py-3 bg-yellow-500 text-white rounded-full 
+      text-lg font-medium shadow-lg transition-all duration-300 
+      hover:bg-yellow-600 hover:scale-105 hover:shadow-2xl">
+        Explore Rooms
+      </button>
+
+    </div>
+
+        
       </header>
     </div>
   );
