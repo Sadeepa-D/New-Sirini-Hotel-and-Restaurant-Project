52c29c2ea040fa02ede8d130fe6aaf657ddfa0c1
Sadeepa Dinakara <sadeepa14dinakara@gmail.com>
2026-03-01 00:00:09 +0530

    implement CRUD operations for food orders with validation

--- New_Sirini_Hotel/Backend/src/routes/RestrauntRoutes.js
+++ New_Sirini_Hotel/Backend/src/routes/RestrauntRoutes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const FoodOrderCont = require("../controllers/Restraunt/FoodOrderCont");
 
 router.post("/placeorder", FoodOrderCont.createFoodOrder);
+router.get("/vieworders", FoodOrderCont.getFoodOrders);
+router.put("/updateorder/:id", FoodOrderCont.editfoodOrder);
+router.delete("/deleteorder/:id", FoodOrderCont.deleteFoodOrder);
 
 module.exports = router;
