d1c6d69d7a8d14842310923ba233ba32e9180025
Sadeepa Dinakara <sadeepa14dinakara@gmail.com>
2026-03-01 22:22:20 +0530

    feat: add FoodItem model with schema definition for restaurant menu items

---
+++ New_Sirini_Hotel/Backend/src/models/Restraunt/FoodItemModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const FoodItemSchema = new mongoose.Schema(
+  {
+    foodname: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    ingredients: {
+      type: [String],
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    availability: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("FoodItem", FoodItemSchema);
