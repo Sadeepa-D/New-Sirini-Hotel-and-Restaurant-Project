70677041e2aec4bf676323a4d0bbc1ff99264a12
Dilshan0721 <kavindu_20221359@fot.ruh.ac.lk>
2026-05-13 07:52:18 +0530

    Create DayUseCalender Component in RoomCompo for get DayTime bookings

--- New_Sirini_Hotel/Frontend/src/Components/RoomCompo/BookingForm.jsx
+++ New_Sirini_Hotel/Frontend/src/Components/RoomCompo/BookingForm.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import RoomCalender from "../RoomCompo/RoomCalender";
 import GuestDetail from "./GuestDetail";
 import BookingSuccess from "../RoomCompo/SuccessMsg";
 
