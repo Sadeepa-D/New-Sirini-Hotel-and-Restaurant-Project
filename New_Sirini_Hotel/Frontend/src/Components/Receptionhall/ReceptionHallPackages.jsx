efa9788b7b95d49a617a304670c52fe7ba959173
Sadeepa Dinakara <sadeepa14dinakara@gmail.com>
2026-05-14 02:31:43 +0530

    Update ReceptionHallPackages and Reception components for improved display and conditional rendering

--- New_Sirini_Hotel/Frontend/src/Components/Receptionhall/ReceptionHallPackages.jsx
+++ New_Sirini_Hotel/Frontend/src/Components/Receptionhall/ReceptionHallPackages.jsx
@@ -97,11 +97,10 @@ export default function ReceptionHallPackages() {
               Special Offer on Hall Charges
             </h4>
             <p className="text-xs text-amber-700">
-              Book for **100+ guests** and get the **Reception Hall for FREE!**{" "}
+              Book for <b>100+ guests</b> and get the <b>Reception Hall for FREE!</b>
               <br />
               <span className="italic text-[10px]">
-                *Less than 100 guests will incur a Rs. 2,000 hall fee per
-                person.*
+                Less than 100 guests will incur a additional hall fee
               </span>
             </p>
           </div>
